445548b990b491941819d04c8ecc163794380db3
root <root@DESKTOP-9G0FDJJ.localdomain>
2025-11-15 17:38:12 +0800

    哈希实现分布式存储

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ import requests
 import json
 from datetime import datetime
 from email.utils import parsedate_to_datetime
+import hashlib
 
 app = Flask(__name__)
 
 ports = ['9527', '9528', '9529']
 data_store = {}
+
+def hash_str_to_port(s):
+    encoded = s.encode('utf-8')
+    hash_digest = hashlib.sha256(encoded).hexdigest()
+    big_int = int(hash_digest, 16)
+    return big_int % 3 + 9527
 
 @app.route('/', methods=['POST'])
 def write_data():
@@ -18,66 +25,56 @@ def write_data():
         data = request.get_json()
         if not isinstance(data, dict):
             abort(400, description="JSON body must be an object")
-        
-        # for port in ports:
-        #     BASE_URL = f"http://127.0.0.1:{port}/init_write_data"
-        #     response = requests.post(
-        #         BASE_URL,
-        #         headers={'Content-Type': 'application/json'},
-        #         data=json.dumps(data)
-        #     )
-        #     if response.status_code != 204:
-        #         abort(400, description={response.text})
 
         for item in data:
             key, value = item, data[item]
-        now = datetime.now()
-        data_store[key] = [value, now]     
-        print(data_store[key])
+        
+        hash_port = hash_str_to_port(key)
+        
+        if hash_port == args.port:
+            data_store[key] = value
+        else:
+            BASE_URL = f"http://cache-server-{hash_port}:{hash_port}/init_write_data"
+            response = requests.post(
+                BASE_URL,
+                headers={'Content-Type': 'application/json'},
+                data=json.dumps(data)
+            )
         return '', 200  
     except Exception as e:
         abort(400, description=str(e))
         
-# @app.route('/init_write_data', methods=['POST'])
-# def init_write_data():
-#     if not request.is_json:
-#         abort(400, description="Content-Type must be application/json")
-#     try:
-#         data = request.get_json()
-#         if not isinstance(data, dict):
-#             abort(400, description="JSON body must be an object")
-        
-#         data_store.update(data)
-#         return '', 204  
-#     except Exception as e:
-#         abort(400, description=str(e))
+@app.route('/init_write_data', methods=['POST'])
+def init_write_data():
+    if not request.is_json:
+        abort(400, description="Content-Type must be application/json")
+    try:
+        data = request.get_json()
+        if not isinstance(data, dict):
+            abort(400, description="JSON body must be an object")
+        data_store.update(data)
+        return '', 204  
+    except Exception as e:
+        abort(400, description=str(e))
 
 @app.route('/<key>', methods=['GET'])
 def read_data(key):
+    hash_port = hash_str_to_port(key)
     value = ""
-    max_time = datetime.min 
-    for port in ports:
-        if port == args.port:
-            if key in data_store:
-                store_value, time = data_store[key]
-                if time > max_time:
-                    max_time = time
-                    value = store_value  
-        else:
-            BASE_URL = f"http://cache-server-{port}:{port}/init_read_data/{key}"
-            response = requests.get(
-                BASE_URL,
-                headers={'Content-Type': 'application/json'},
-            )
-            content = response.json() 
-            if content != "":
-                store_value, time = content
-                time = parsedate_to_datetime(time).replace(tzinfo=None)
-                if time > max_time:
-                    max_time = time
-                    value = store_value 
+    if hash_port == args.port:
+        if key in data_store:
+            value = data_store[key]
+    else:
+        BASE_URL = f"http://cache-server-{hash_port}:{hash_port}/init_read_data/{key}"
+        response = requests.get(
+            BASE_URL,
+            headers={'Content-Type': 'application/json'},
+        )
+        content = response.json() 
+        if content != "":
+            value = content
 
-    if max_time != datetime.min:
+    if value != "":
         return jsonify({key: value})
     else:
         return '', 404
@@ -92,20 +89,20 @@ def init_read_data(key):
 @app.route('/<key>', methods=['DELETE'])
 def delete_data(key):
     flag = False
-    for port in ports:
-        if port == args.port:
-            if key in data_store:
-                flag = True
-                data_store.pop()
-        else:
-            BASE_URL = f"http://cache-server-{port}:{port}/init_delete_data/{key}"
-            response = requests.delete(
-                BASE_URL,
-                headers={'Content-Type': 'application/json'}
-            )
-            content = response.json()
-            if content:
-                flag = True
+    hash_port = hash_str_to_port(key)
+    if hash_port == args.port:
+        if key in data_store:
+            flag = True
+            data_store.pop(key)
+    else:
+        BASE_URL = f"http://cache-server-{hash_port}:{hash_port}/init_delete_data/{key}"
+        response = requests.delete(
+            BASE_URL,
+            headers={'Content-Type': 'application/json'}
+        )
+        content = response.json()
+        if content:
+            flag = True
     if flag:
         return jsonify(1)
     else:
